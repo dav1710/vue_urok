0297287042258ba536ef86d89dda63609b4f221c
unknown <you@example.com>
2024-07-07 22:57:43 +0400

    07.07 home

---
+++ vue-project/src/components/TheHeaderProgress.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+</script>
+
+<template>
+      <div v-if="true" class="flex items-center gap-1">
+        Day complete!
+        <CheckCircleIcon class="h-7 text-green-500" />
+      </div>
+      <div v-else class="flex items-center gap-1">
+        <div>Progress: <span class="font-mono">20%</span></div>
+        <div class="h-3 w-3 rounded-full bg-red-500"></div>
+      </div>
+</template>
