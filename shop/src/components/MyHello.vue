b858b21c080b2a3a2f8c25ecde276350f028818a
Davit_Muradyan <davit.muradyan@locator.am>
2024-07-19 18:06:07 +0400

    17.07 office

---
+++ shop/src/components/MyHello.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  title: String,
+  price: Number,
+  isAdded: Boolean
+});
+</script>
+
+<template>
+  <div class="product">
+    <h2>Product: {{ title }}</h2>
+    <h3>Price: {{ price }}</h3>
+    <h3>Added: {{ isAdded }}</h3>
+  </div>
+</template>
+
+<style scoped>
+.product {
+  border: 1px solid black;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</style>
